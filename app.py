23ff5ea324e2db02814fb804d481074172b354ea
Benjamin Baranger <baranger.benjamin@gmail.com>
2020-12-05 18:39:42 +0000

    day 2 done

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 f = open("input.txt", "r")
 passwords = [x for x in f]
 
-def is_valid(minimum, maximum, letter, passwd):
-  if passwd.count(letter) >= minimum and passwd.count(letter) <= maximum:
+def is_valid(pos1, pos2, letter, passwd):
+  if passwd[pos1] == letter and passwd[pos2] == letter:
+    return False
+  elif passwd[pos1] == letter:
     return True
+  elif passwd[pos2] == letter:
+    return True
+  
 
 count = 0
 for password in passwords:
   letter = password.replace(":", '').split(" ")[1]
   passwd = password.replace(":", '').split(" ")[2]
-  minimum = int(password.replace(":", '').split(" ")[0].split('-')[0])
-  maximum = int(password.replace(":", '').split(" ")[0].split('-')[1])
+  pos1 = int(password.replace(":", '').split(" ")[0].split('-')[0])
+  pos2 = int(password.replace(":", '').split(" ")[0].split('-')[1])
   
-  if is_valid(minimum, maximum, letter, passwd):
+  if is_valid(pos1 - 1, pos2 - 1, letter, passwd):
     count += 1
 
 print(count)
